a2ba2e98ede5f5ceec16481271ff66c894102305
unknown <beny11min@gmail.com>
2024-03-30 20:19:34 +0330

    add a round randomly

--- poker.py
+++ poker.py
@@ -1,18 +1,19 @@
 import random
-class cards:
+class Cards:
     number = ['A', 'K', 'Q', 'J', '10', '9', '8', '7','6', '5', '4', '3', '2']
-    suits = ['-h', '-s', '-c', '-d']
+    suits = ['♥', '♠', '♣', '♦']
+    #♣♠♦♥
     deck = []
     def __init__(self):
-        for card in self.number:  
+        for c in self.number:
             for suit in self.suits:  
-                self.deck.append(card + suit )  
-
-    def shuffle(self):
-        pass
+                self.deck.append(c + suit )
 
 
-class player:
+
+
+
+class Player:
     def __init__(self, name: str, cards: list, chips: int):
         self.name = name
         self.cards = cards
@@ -22,9 +23,12 @@ class player:
         self.playerType = 'default'
 
     def action(self, community_cards = None,):
+
         if(toCall == bigBlind):
-            actions = ["check", "bet", "raise", "fold"]
-            choice = random.randint(0, len(actions))
+            actions = ["check", "bet", "raise", "fold", "call"]
+            choice = random.randint(0, len(actions)-1)
+            if (actions[choice] == "call"):
+                return actions[choice], toCall
             if(actions[choice] != "check" and actions[choice] != "fold" ):
                 #bet
                 bet_size = random.randint(bigBlind*2, self.chips)
@@ -32,8 +36,11 @@ class player:
             elif(actions[choice] == "check" or actions[choice] == "fold"):
                 return  actions[choice], 0
         else:
-            actions = ["raise", "fold"]
-            choice = random.randint(0, len(actions))
+            #raised before
+            actions = ["raise", "fold", "call"]
+            choice = random.randint(0, len(actions)-1)
+            if (actions[choice] == "call"):
+                return actions[choice], toCall
             if(actions[choice] == "raise"):
                 #raise 2 times the previous raiser
                 bet_size = random.randint(toCall * 2, self.chips)
@@ -45,7 +52,7 @@ class player:
 
 
 
-    
+    #TODO: add draw function
     def hand_strength(self):
         pass
 
@@ -76,16 +83,16 @@ def assign_pos(players):
 
 
 
-card  = cards()
 
 
 
-p2 = player('bot-1', ['J-h','J-d'],1000)
-p3 = player('bot-2', ['A-h', 'A-d'], 1000)
-p4 = player('bot-3', [], 1000)
-p5 = player('bot-4', [], 1000)
-p6 = player('bot-5', [], 1000)
-p1 = player('player', [], 1000)
+
+p2 = Player('bot-1', [],1000)
+p3 = Player('bot-2', [], 1000)
+p4 = Player('bot-3', [], 1000)
+p5 = Player('bot-4', [], 1000)
+p6 = Player('bot-5', [], 1000)
+p1 = Player('player', [], 1000)
 
 
 players = [p1, p2, p3, p4, p5, p6]
@@ -112,47 +119,68 @@ players_queue.put(players[0])
 players_queue.put(players[1])
 players_queue.put(players[2])
 
-#while playing:
- #   def assign_pos(p1, p2, p3)
-    #pay_blinds()
-     
-    #under the gun action 
-                #blinds # bet   #call # sb raise bb call d re raise 
-    
+
+card  = Cards()
+deck = card.deck
+random.shuffle(deck)
+print(deck)
+print(deck.pop(0))
+print(deck.pop(0))
+print(deck)
 smallBlind = 5
 bigBlind = 10
 Pot = 0
 toCall = bigBlind
 #actions = ["check", "bet", "raise", "fold"]
-last_player = players[2].name
+last_player = players[2].name  # bigblind last player to act
+firstTimeFlag = True
 
 
-def pre_flop_action(players_queue):
-    '''
-    pre flop loop goes here 
-    '''
+def pre_flop_action(toCall= toCall, players_queue= players_queue, firstTimeFlag= firstTimeFlag, last_player = last_player):
+
     #pay blinds()
+
+
     for i, p in enumerate(players):
         if(p.pos == "SB"):
             p.chips -= smallBlind
-            p.paid_round += smallBlind
+            p.set_paid_round(smallBlind)
         if(p.pos == "BB"):
             p.chips -= bigBlind
-            p.paid_round += bigBlind
+            p.set_paid_round(bigBlind)
 
-    while(roundFlag):
+    while(True):
+
         p = players_queue.get()
+        if (last_player == p.name and (not firstTimeFlag)) :
+            # end the round
+            print("round is over ..")
+            break
         action, bet_size = p.action()
+        if(firstTimeFlag):
+            firstTimeFlag = False
+
         if(action == "fold"):
-            #reset player?
+            #reset player?()
             continue
 
-        elif(action == "bet" or action == "raise")
+        elif(action == "bet" or action == "raise"):
+            p.chips -= bet_size
+            p.set_paid_round(bet_size)
+            toCall = bet_size
+            players_queue.put(p)
+            last_player = p.name
+
+        elif(action == "call"):
+            p.chips -= toCall
+            p.set_paid_round(toCall)
+            players_queue.put(p)
+
     #check action 
     #p.set_paid_round()                
 
     # add player  to end of the Q
-    players_queue.put(p)
+
 
     #check if paid is equal
     #True, False function()
@@ -160,4 +188,7 @@ def pre_flop_action(players_queue):
     
     #pay 20  to call = max - pay = 80
     #pay 50  to call 50     paid hame barabar bood
-    #pay 100  
+    #pay 100
+pre_flop_action(toCall, players_queue, firstTimeFlag, last_player)
+
+
